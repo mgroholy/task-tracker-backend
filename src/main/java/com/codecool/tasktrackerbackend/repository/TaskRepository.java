0d1b5e8f6a1a24a44f9122c1c25873209c0da851
Máté Groholy <mate.groholy@gmail.com>
2021-06-15 10:26:57 +0200

    fix: fix repository Type declaration.

--- src/main/java/com/codecool/tasktrackerbackend/repository/TaskRepository.java
+++ src/main/java/com/codecool/tasktrackerbackend/repository/TaskRepository.java
@@ -3,5 +3,5 @@ package com.codecool.tasktrackerbackend.repository;
 import com.codecool.tasktrackerbackend.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Long, Task> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
