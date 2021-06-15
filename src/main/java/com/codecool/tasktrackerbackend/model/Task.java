ceb92a6d17272cd5300ab467a03a0815f4a831d3
Máté Groholy <mate.groholy@gmail.com>
2021-06-15 10:27:46 +0200

    fix: modify Task model. day field is LocalDateTime type.

--- src/main/java/com/codecool/tasktrackerbackend/model/Task.java
+++ src/main/java/com/codecool/tasktrackerbackend/model/Task.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -22,7 +23,7 @@ public class Task {
     private String text;
 
     @NonNull
-    private LocalDate day;
+    private LocalDateTime day;
 
     private boolean reminder;
 
