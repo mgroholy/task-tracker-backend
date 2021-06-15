699150a6035cab0195fa233555678d7cf5f95eca
Máté Groholy <mate.groholy@gmail.com>
2021-06-15 13:37:35 +0200

    feat: create deleteTask methon in TaskService. Throw NoSuchElementException if task is not found.

--- src/main/java/com/codecool/tasktrackerbackend/service/TaskService.java
+++ src/main/java/com/codecool/tasktrackerbackend/service/TaskService.java
@@ -3,9 +3,11 @@ package com.codecool.tasktrackerbackend.service;
 import com.codecool.tasktrackerbackend.model.Task;
 import com.codecool.tasktrackerbackend.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class TaskService {
@@ -23,5 +25,13 @@ public class TaskService {
 
     public Task addTask(Task task) {
         return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        try{
+            taskRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception){
+            throw new NoSuchElementException(String.format("No task found with id: %d", id));
+        }
     }
 }
